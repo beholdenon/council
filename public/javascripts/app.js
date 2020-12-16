e1e6f01a95d0fed52e062f61bd9c89dc453d357a
Brian Holden <brian.holden@bloomingdales.com>
2020-12-16 08:49:14 -0500

    Fixed hp form

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,10 +1,32 @@
 $(function() {
-	$( "form" ).on( "submit", function( event ) {
+	$( "#hp-join-form" ).on( "submit", function( event ) {
 	  event.preventDefault();
-	  var data = $( this ).serialize();
-	  $.post( "https://holdenforcitycouncil.us7.list-manage.com/subscribe/post?u=7d00529cc08ebc48d83096c4f&amp;id=5e3c61dfbc", data)
-		  .done(function( data ) {
-		    alert( "Data Loaded: " + data );
-		  });
+		$.ajax({
+			type: $( this ).attr('method'),
+			url: $( this ).attr('action'),
+			data: $( this ).serialize(),
+			cache: false,
+			dataType: 'json',
+			contentType: 'application/json; charset=utf-8',
+			error: function (err) { console.log(err); },
+			success: function (data) {
+				console.log(data);
+				if (data.result === 'success') {
+					$( "#hp-join-form" ).hide();
+					$( '#hp-join-form-message').html("Thank you for signing up!");
+				}
+				else {
+					if(!validateEmail($(' #hp-join-form #mce-EMAIL').val())) {
+						$(' #hp-join-form #mce-EMAIL').addClass('error');
+						$( '#hp-join-form-message').html("Please enter a valid email address.");
+					}
+				}
+			},
+		});
 	});
+
+	function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+	}
 });
