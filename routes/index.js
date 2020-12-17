66adcf07303a3b67b4509b376955541ce368d416
Brian Holden <brian.holden@bloomingdales.com>
2020-12-17 17:18:18 -0500

    Add hp to contentful

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var news = require('../services/news.js')
+var homepage = require('../services/homepage.js')
 
 router.use(function (req, res, next) {
-  news.getNews().then(function (newsCollection) {
-    req.news = newsCollection.items;
+  homepage.getHomepage().then(function (homepageCollection) {
+    req.homepage = homepageCollection.items;
+    console.log(JSON.stringify(req.homepage));
     next();
   }).catch(function (err) {
-    console.log('news.js - getNews (line 23) error:', JSON.stringify(err,null,2))
+    console.log('homepage.js - getHomepage (line 23) error:', JSON.stringify(err,null,2))
     next();
   });
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	console.log(req.news);
-	res.render('index', { 'news': req.news, 'title': 'Re-Elect Robert Holden For City Council - District 30' });
+	const absoluteRoot = req.protocol + '://' + req.get('host');
+	res.render('index', { 'homepage': req.homepage, 'url': absoluteRoot + req.url, 'image': absoluteRoot + '/images/holden-feature-desktop.jpg', 'title': 'Re-Elect Robert Holden For City Council - District 30' });
 });
 
 module.exports = router;
