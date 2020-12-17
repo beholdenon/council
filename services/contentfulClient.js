cfa95a129a0c54b698ba4779ba9f057d0bae7384
Brian Holden <brian.holden@bloomingdales.com>
2020-12-16 20:27:09 -0500

    Hiding api keys

--- services/contentfulClient.js
+++ services/contentfulClient.js
@@ -1,9 +1,15 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+console.log(process.env.CONTENTFUL_API_TOKEN);
+
 var contentful = require('contentful')
 var config = require('../package.json').config || {}
 
 var client = contentful.createClient({
-  accessToken: config.accessToken,
-  space: config.space
+  accessToken: process.env.CONTENTFUL_API_TOKEN,
+  space: process.env.CONTENTFUL_SPACE_ID
 })
 
 exports.client = client
